64d37397c13efc0ca3537474672f74a5e836c8db
Álvaro Barbero <albaol60n@gmail.com>
2024-09-01 19:50:59 +0200

    Added Instagram preview

--- frontend/src/Components/InstagramPreview.js
+++ frontend/src/Components/InstagramPreview.js
@@ -2,16 +2,21 @@
 import React from 'react';
 
 function InstagramPreview ({ metadata }) {
-  const { title, image, url } = metadata;
+  const { title, description, image, url } = metadata;
 
   return (
-    <div className="instagram-preview p-4 border rounded-lg bg-white shadow-md">
-      {image && <img src={image} alt={title} className="w-full h-32 object-cover rounded-md" />}
-      <div className="mt-2">
-        <h3 className="font-bold text-lg">{title}</h3>
-        <a href={url} className="text-blue-500 hover:underline">{url}</a>
+    <div className='w-full flex flex-col justify-center items-center'>
+      <h2 className='font-bold text-lg mt-10 text-center'>Previsualización en Instagram</h2>
+      <div className="w-11/12 border rounded-lg bg-white shadow-md sm:w-9/12 md:w-1/2 lg:w-4/12">
+        {image && <img src={image} alt={title} className="w-full h-32 object-cover rounded-md" />}
+        <div className="mt-2 mx-2">
+          <h3 className="font-bold text-lg">{title}</h3>
+          <p className="text-sm text-gray-700 truncate">{description}</p>
+
+          <a href={url} className="text-blue-500 hover:underline">{url}</a>
+        </div>
       </div>
-    </div>
+    </div >
   );
 }
 
